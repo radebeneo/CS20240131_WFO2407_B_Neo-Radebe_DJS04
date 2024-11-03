822855e7bb4026cb24b2bfca6deb3c6484fca3f6
umkhulubhungane <admin@Apple-MacBook-Pro-Retina-13-Early-2015.local>
2024-11-03 16:51:10 +0200

    Render the component's HTML and CSS when added to the page.

--- bookpreview.js
+++ bookpreview.js
@@ -5,3 +5,8 @@ class BookPreview extends HTMLElement {
         // Attach a shadow DOM to this element, keeping styles and structure encapsulated
         this.attachShadow({ mode: 'open' });
     }
+
+    // This lifecycle method is called when the component is added to the DOM
+    connectedCallback() {
+        this.render(); // Render the component's HTML and CSS when added to the page
+    }
