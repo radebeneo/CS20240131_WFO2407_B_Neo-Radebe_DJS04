c1fcb9537e7a3fef119abe18e849251bf0d3aadc
umkhulubhungane <admin@Apple-MacBook-Pro-Retina-13-Early-2015.local>
2024-11-03 16:49:36 +0200

    Attach a shadow DOM to this element, keeping styles and structure encapsulated.

--- bookpreview.js
+++ bookpreview.js
@@ -2,3 +2,6 @@ class BookPreview extends HTMLElement {
     // Constructor to set up the component
     constructor() {
         super();
+        // Attach a shadow DOM to this element, keeping styles and structure encapsulated
+        this.attachShadow({ mode: 'open' });
+    }
